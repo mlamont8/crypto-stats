337b070755ad00eaabe8309f07d57c0ff0741f29
mlamont8 <debiandmiat@hotmail.com>
2018-03-11 20:55:18 -0400

    minor update

--- src/components/summary/summary.js
+++ src/components/summary/summary.js
@@ -6,6 +6,7 @@ return (
     <div className='container-fluid summary'>
         <div className ='row'>
             <div className='col-sm-2 summary-left'>
+            
             </div>
             <div className='col-sm-8 summary-center'>
             </div>
