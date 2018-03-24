3b547dde0cfd3cc91218b6e55a4763d67c94b15b
mlamont8 <debiandmiat@hotmail.com>
2018-03-24 15:53:09 -0400

    completed pie chart functionality

--- src/components/exchangeVolume/exchangeVolume.js
+++ src/components/exchangeVolume/exchangeVolume.js
@@ -8,7 +8,8 @@ const exchangeVolume = (props) => {
   return (
     <ResponsiveContainer width="100%" height={300}>
       <PieChart width={730} height={250}>
-          <Pie data={props.data} dataKey="VOLUME24HOUR" nameKey="MARKET" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
+          <Pie data={props.data} dataKey="VOLUME24HOUR" nameKey="MARKET" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d"
+          label={({MARKET})=>`${MARKET}`} />
       </PieChart>
     </ResponsiveContainer>
   )
