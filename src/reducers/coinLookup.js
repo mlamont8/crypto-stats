1dbb7cb5fa2551cdfbe5f6804935dccfcd9e14e0
mlamont8 <debiandmiat@hotmail.com>
2018-03-26 09:52:09 -0400

    fixed coinlookup bug when coin undefined

--- src/reducers/coinLookup.js
+++ src/reducers/coinLookup.js
@@ -1,9 +1,16 @@
 const CoinLookup = (state = {}, action) => {
     switch (action.type) {
         case 'COIN_LOOKUP':
+            console.log(action.coin)
+            if (action.coin) {
             return {
                 ...state,
                 [action.id]: action.coin.CoinName
+            }
+         }
+            return {
+                ...state,
+                [action.id]: ""
             }
 
         default:
