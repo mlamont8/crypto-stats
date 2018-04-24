04a9238278aaae18457ebd12840bde758b9c59d9
mlamont8 <mlamont8@hotmail.com>
2018-04-23 21:26:07 -0400

    initial setup for sagas

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,16 +1,23 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+import cryptoApp from "../reducers";
+import mySaga from "../sagas";
 
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import cryptoApp from '../reducers';
-
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware();
 
 const configureStore = () => {
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const store = createStore(
     cryptoApp,
-    composeEnhancers(applyMiddleware(thunk)),
+    composeEnhancers(applyMiddleware(thunk, sagaMiddleware))
   );
   return store;
 };
+
+// then run the saga
+sagaMiddleware.run(mySaga);
 
 export default configureStore;
