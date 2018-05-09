1560dce8208e965a043651192e1fd8ed8f55d242
mlamont8 <debiandmiat@hotmail.com>
2018-05-09 15:06:19 -0400

    reduced size of navbar

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -11,14 +11,6 @@ const Header = () => (
       <Navbar.Toggle />
     </Navbar.Header>
     <Navbar.Collapse>
-      <Nav>
-        <NavItem eventKey={1} href="#">
-          Link
-        </NavItem>
-        <NavItem eventKey={2} href="#">
-          Link
-        </NavItem>
-      </Nav>
       <Nav pullRight>
         <NavItem eventKey={1} href="#">
           Link Right
