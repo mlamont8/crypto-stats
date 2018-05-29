9afd99128b95fe7422b10a6cbcc21a765e726c1b
mlamont8 <debiandmiat@hotmail.com>
2018-05-29 18:25:26 -0400

    added test for action

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -1,24 +1,15 @@
-import configureMockStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
-import * as actions from './index'
-
- 
-const middlewares = [thunk]
-const mockStore = configureMockStore(middlewares)
- 
+import { SelectData } from './index'
 
 describe('actions', () => {
- 
-  it('creates FETCH_TODOS_SUCCESS when fetching todos has been done', () => {
-
-    const store = mockStore({})
-      return store.dispatch(actions.fetchCoinList()).then(() => {
-        const actions = store.getActions()
-      // return of async actions
-      expect(actions[1].type).toEqual('FRONT_FETCH_DATA_SUCCESS')
-    })
+  it('should create an action to do a search', () => {
+    const id = "market";
+    const item = "CoinBase";
+    const expectedAction = {
+      type: "SELECTION_ENTERED",
+      id,
+      item
+    }
+    expect(SelectData(id, item)).toEqual(expectedAction);
   })
-
-
-
 })
+
