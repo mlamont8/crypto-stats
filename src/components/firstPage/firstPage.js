c8e43f3cb19fe0da6c6a4ebc729f01ac4ec3804b
mlamont8 <debiandmiat@hotmail.com>
2018-06-03 19:08:27 -0400

    updated front page css

--- src/components/firstPage/firstPage.js
+++ src/components/firstPage/firstPage.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const FirstPage = () => (
     <div className="container-fluid firstPage">
-        <span> FIRST PAGE </span>
-        <span> First render </span>
+        <div className="firstPageInfo">
+            <p>FIRST PAGE</p>
+            <p>First render</p>
+        </div>
     </div>
 )
 
