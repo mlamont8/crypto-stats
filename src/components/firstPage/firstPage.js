672dd474e5e5e95769a8a6b39751c82d75cc7331
mlamont8 <mlamont8@hotmail.com>
2018-06-05 21:16:58 -0400

    updated front page with form

--- src/components/firstPage/firstPage.js
+++ src/components/firstPage/firstPage.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
+import CoinForm from "../../containers/coinForm/CoinForm";
 
 const FirstPage = () => (
-    <div className="container-fluid firstPage">
-        <div className="firstPageInfo">
-            <p>FIRST PAGE</p>
-            <p>First render</p>
-        </div>
+  <div className="container-fluid firstPage">
+    <div className="firstPageInfo col-md-offset-3 col-md-6">
+      <CoinForm />
     </div>
-)
+  </div>
+);
 
 export default FirstPage;
