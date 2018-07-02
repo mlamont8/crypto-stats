b6ec1e3291111eb5290b9d5d489254d7a01472c1
mlamont8 <mlamont8@hotmail.com>
2018-07-01 20:52:52 -0400

    added notification for notification status change

--- src/containers/header/header.js
+++ src/containers/header/header.js
@@ -1,47 +1,78 @@
 import React from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import { notificationOption } from "../../actions"
+import { notificationOption } from "../../actions";
+import { notificationAlert } from "../../helpers/index";
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.updateNotificationStatus = this.updateNotificationStatus.bind(this);
+  }
+
+  // Notify user and make notification status chnage
+  updateNotificationStatus() {
+    const { toggle } = this.props;
+    const msg = `NOTIFICATIONS: ${toggle}`;
+    this.props.changeNotification();
+    notificationAlert(msg, "alert");
+  }
 
   render() {
     return (
       <nav className="uk-navbar-container" uk-navbar="true">
         <div className="uk-navbar-left">
-
           <span className="uk-navbar-item uk-logo">Crypto Stats</span>
-
         </div>
 
         <div className="uk-navbar-right">
           <div className="uk-margin uk-grid-small uk-child-width-auto">
             <label>Notifications</label>
-            <label><input className="uk-radio" value="on" type="radio" onChange={this.props.changeNotification} checked={this.props.toggle === "on"} /> On</label>
-            <label><input className="uk-radio" value="off" type="radio" onChange={this.props.changeNotification} checked={this.props.toggle === "off"} /> Off</label>
+            <label>
+              <input
+                className="uk-radio"
+                value="on"
+                type="radio"
+                onChange={this.updateNotificationStatus}
+                checked={this.props.toggle === "on"}
+              />{" "}
+              On
+            </label>
+            <label>
+              <input
+                className="uk-radio"
+                value="off"
+                type="radio"
+                onChange={this.updateNotificationStatus}
+                checked={this.props.toggle === "off"}
+              />{" "}
+              Off
+            </label>
           </div>
         </div>
       </nav>
-    )
-  };
-};
+    );
+  }
+}
 
 const mapStateToProps = state => {
   return {
     toggle: state.notification.option
-  }
-}
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
     changeNotification: () => {
-      dispatch(notificationOption())
+      dispatch(notificationOption());
     }
-  }
-}
+  };
+};
 
 Header.propTypes = {
-  firstLoad: PropTypes.bool
+  firstLoad: PropTypes.bool,
+  changeNotification: PropTypes.func.isRequired,
+  toggle: PropTypes.string.isRequired
 };
 
 Header.defaultProps = {
