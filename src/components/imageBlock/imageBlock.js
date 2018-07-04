1b66f3ebc03ff7fb46829d381e6338b669b89b6f
mlamont8 <debiandmiat@hotmail.com>
2018-07-04 14:06:37 -0400

    updated imageblock container to flex

--- src/components/imageBlock/imageBlock.js
+++ src/components/imageBlock/imageBlock.js
@@ -1,26 +1,25 @@
 import React from "react";
-import { Image } from 'react-bootstrap'
 import VertForm from '../../containers/vertForm/vertForm'
 
 const ImageBlock = (props) => (
-    <div>
-        <div className="row">
-            <div className="col-xs-6">
-                <Image src={props.img} circle responsive />
-            </div>
-        </div>
-        <div className="row">
-            <div className="col-xs-12">
+
+    <div className="ibContainer uk-flex uk-flex-column">
+        <div className="ibContainer-fr uk-flex uk-flex-around uk-flex-middle">
+
+            <img className="imageIcon" data-src={props.img} width="40px" height="40px" uk-img="true" />
+            <span className="nameBlk uk-flex uk-flex-center">
                 <h1>{props.coinFrom}</h1>
-                <h2>{props.coinName}</h2>
-            </div>
+            </span>
+
         </div>
-        <div className="row">
-            <div className="col-xs-12">
-                <VertForm />
-            </div>
+        <div className="ibContainer-sr">
+            <span><h2>{props.coinName}</h2></span>
         </div>
-    </div >
+
+        <div className="ibContainer-tr">
+            <VertForm />
+        </div>
+    </div>
 )
 
 export default ImageBlock;
