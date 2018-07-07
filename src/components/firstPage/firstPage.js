8b4a87283663a58bd945586493955cead5ac1077
mlamont8 <debiandmiat@hotmail.com>
2018-07-06 20:43:30 -0400

    adjusted frontpage layout

--- src/components/firstPage/firstPage.js
+++ src/components/firstPage/firstPage.js
@@ -2,9 +2,9 @@ import React from "react";
 import CoinForm from "../../containers/coinForm/CoinForm";
 
 const FirstPage = () => (
-  <div className="container-fluid firstPage">
-    <div className="frontLeft col-xs-6" />
-    <div className="frontRight col-xs-6">
+  <div className="frontContainer">
+    <div className="frontLeft " />
+    <div className="frontRight ">
       <div className="firstPageInfo">
         <CoinForm />
       </div>
