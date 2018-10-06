940b37e4b7e76257d46064e2c337921757e17ebf
mlamont8 <debiandmiat@hotmail.com>
2018-10-06 10:48:54 -0400

    updated ImageBlock spacing and design

--- src/components/imageBlock/imageBlock.js
+++ src/components/imageBlock/imageBlock.js
@@ -2,8 +2,7 @@ import React from "react";
 import VertForm from "../../containers/vertForm/vertForm";
 
 const ImageBlock = props => (
-  <div className="ibContainer infoBlock">
-    <div className="ibContainer-fr uk-flex uk-flex-around uk-flex-middle">
+  <div className="infoBlock imageBlock">
       <img
         className="imageIcon"
         alt={props.coinFrom}
@@ -12,19 +11,19 @@ const ImageBlock = props => (
         height="40px"
         uk-img="true"
       />
-      <span className="nameBlk uk-flex uk-flex-center">
+      <span className="nameBlk">
         <h1>{props.coinFrom}</h1>
       </span>
-    </div>
-    <div className="ibContainer-sr">
-      <span>
-        <h2>{props.coinName}</h2>
-      </span>
-    </div>
+   
+      <div className="ibContainer-sr">
+        <span>
+          <h2>{props.coinName}</h2>
+        </span>
+      </div>
 
-    <div className="ibContainer-tr">
-      <VertForm />
-    </div>
+      <div className="ibContainer-tr">
+        <VertForm />
+      </div>
   </div>
 );
 
