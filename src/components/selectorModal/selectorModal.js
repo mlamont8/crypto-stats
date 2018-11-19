559fe4a8a42969a687b8c87eb662b8f0820968cb
Lamont <mlamont8@hotmail.com>
2018-11-18 19:25:11 -0500

    changed SelectorModal to class and container

--- src/components/selectorModal/selectorModal.js
+++ src/components/selectorModal/selectorModal.js
@@ -1,18 +1,44 @@
 import React from 'react';
+import { connect } from "react-redux";
 
 
-const SelectorModal = (props) => {
+class SelectorModal extends React.Component {
 
-
-    return (
-        <div className="modalContainer">
-            <div className="modalContent">
-                Modal Content
-                <button type="button" onClick={props.handleClose}>Close</button>
+    render() {
+        const { market, convertFrom, convertTo, marketArray, fromArray, toArray } = this.props;
+        let currentArray = {};
+        console.log({ market });
+        console.log({ marketArray });
+        if (market === false) { currentArray = marketArray; }
+        console.log('currentArray', currentArray);
+        return (
+            <div className="modalContainer">
+                <div className="modalContent">
+                    Modal Content
+                <button type="button" onClick={this.props.handleClose}>Close</button>
+                </div>
             </div>
-        </div>
-    )
-
+        )
+    }
 }
 
-export default SelectorModal;
+const mapStateToProps = state => {
+    return {
+        market: state.searchTerm.market,
+        convertFrom: state.searchTerm.convertFrom,
+        convertTo: state.searchTerm.convertTo,
+        marketArray: state.searchArrays.marketArray,
+        fromArray: state.searchArrays.convertFrom,
+        toArray: state.searchArrays.convertTo,
+    };
+};
+
+// const mapDispatchToProps = dispatch => {
+//     return {
+//         changeNotification: () => {
+//             dispatch(notificationOption());
+//         }
+//     };
+// };
+
+export default connect(mapStateToProps, null)(SelectorModal);
