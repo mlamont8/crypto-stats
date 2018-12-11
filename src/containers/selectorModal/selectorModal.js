0fc3cb45a9ad8f424afae13f8da8c8bb91211a03
mlamont8 <debiandmiat@hotmail.com>
2018-12-11 08:52:26 -0500

    added headers to modal

--- src/containers/selectorModal/selectorModal.js
+++ src/containers/selectorModal/selectorModal.js
@@ -3,21 +3,19 @@ import { connect } from "react-redux";
 import { SelectData, modalState, idUpdate } from "../../actions";
 
 // Receives props from state tree
+// Child of firstPage.js and Main.js
 
 export class SelectorModal extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            id: '',
-        }
         this.onOptionClick = this.onOptionClick.bind(this);
     }
 
     // Updates to new id after click
     onOptionClick(item) {
 
-        const id = this.props.id;
+        const { id } = this.props;
         // send item with current id
         this.props.optionClick(id, item)
         if (id === 'market') {
@@ -37,20 +35,51 @@ export class SelectorModal extends React.Component {
         }
     }
 
+    // Offers user instructions in Modal Header
+    instructs() {
+        const { id } = this.props;
+        if (id === 'market') {
+
+            return "Choose a MARKET"
+
+        } else if (id === 'convertFrom') {
+            return "Choose the coin you are CONVERTING FROM"
+        }
+        else {
+            return "Choose the coin you are CONVERTING TO"
+        }
+
+    }
+
     render() {
         // get object from state store
         const { currentArray } = this.props;
+        const instructions = this.instructs();
+        console.log('currentArray', currentArray)
         return (
             <div className="modalContainer">
                 <div className="modalContent">
+
+                    <div className="modalHeader">
+                        Header
+                    </div>
+                    <div className="modalInstructions">
+                        {instructions}
+                    </div>
+
+                    <div className="modalAlerts">
+                        Alert
+                    </div>
                     <div className="selectorItems">
                         {currentArray.sort().map(item =>
                             <div key={item}>
                                 <a onClick={this.onOptionClick.bind(this, { item }.item)}>{item}</a>
                             </div>)}
                     </div>
-                    <button type="button" onClick={this.props.modalStatus.bind(this, false)}>Close</button>
-                    <button type="button" onClick={this.props.searchReset}>Reset</button>
+                    <div className="modalFooter">
+                        <button type="button" onClick={this.props.modalStatus.bind(this, false)}>Close</button>
+                        <button type="button" onClick={this.props.searchReset}>Reset</button>
+                    </div>
                 </div>
             </div>
         )
