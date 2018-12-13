c37ab7065bbef2506fc8a66acabd2e689a0ace63
mlamont8 <debiandmiat@hotmail.com>
2018-12-12 20:10:21 -0500

    updated front page design

--- src/components/firstPage/firstPage.js
+++ src/components/firstPage/firstPage.js
@@ -12,19 +12,17 @@ export class FirstPage extends React.Component {
         {this.props.modal &&
           <SelectorModal />
         }
-        <div className="frontLeft " />
-        <div className="frontRight ">
-          <div className="firstPageInfo">
-          </div>
-          <div className="modalButton">
-            <button type="button" onClick={this.props.modalToggle.bind(this, true)}>
-              Get Started
-            </button>
 
-          </div>
+        <div className="frontViewArea">
+
+          <svg id="curve" width="1400" height="300" xmlns="http://www.w3.org/2000/svg" svg="http://www.w3.org/2000/svg">
+            <path d="m-0.60241,236.74699c336.14458,-68.6747 578.31326,-30.12049 734.93976,16.86747c156.6265,46.98795 687.95181,-46.98795 687.3494,-47.59036c-0.60241,-0.60241 -9.03615,104.21686 -9.63856,103.61445c-0.60241,-0.60241 -1412.6506,9.03615 -1412.04819,8.43374c0.60241,-0.60241 -336.74699,-12.6506 -0.60241,-81.3253z" fill="#FF0000" id="svg_1" />
+          </svg>
         </div>
-
       </div>
+
+
+
 
     )
   }
