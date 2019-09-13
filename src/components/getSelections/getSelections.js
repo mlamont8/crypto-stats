d2fe4bb987c9887db23193c22ea18363c4f80228
mlamont8 <mlamont8@hotmail.com>
2019-09-13 17:53:00 -0400

    removed unused component

--- src/components/getSelections/getSelections.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useSelector } from "react-redux";
-
-function GetSelections(props) {
-  const selections = useSelector(state => state.searchArrays.currentArray);
-  console.log({ selections });
-  return true;
-}
-export default GetSelections;
--- src/containers/chatForm/ChatForm.js
+++ src/containers/chatForm/ChatForm.js
@@ -1,7 +1,5 @@
-import React, { useEffect } from "react";
+import React from "react";
 import ChatBot from "react-simple-chatbot";
-import GetSelections from "../../components/getSelections/getSelections";
-import { useSelector } from "react-redux";
 import ChatSelector from "../../components/chatSelector/chatSelector";
 
 function ChatForm() {
@@ -22,13 +20,9 @@ function ChatForm() {
           id: "market",
           component: <ChatSelector id={"market"} />,
           waitAction: true,
-          trigger: "coin-retrieve"
-        },
-        {
-          id: "coin-retrieve",
-          component: <GetSelections id={"convertFrom"} />,
           trigger: "coin-question"
         },
+
         {
           id: "coin-question",
           message: "Great! Which Coin would you like info on?",
@@ -38,11 +32,6 @@ function ChatForm() {
           id: "convertFrom",
           component: <ChatSelector id={"convertFrom"} />,
           waitAction: true,
-          trigger: "retrieve-conversion"
-        },
-        {
-          id: "retrieve-conversion",
-          component: <GetSelections id={"convertTo"} />,
           trigger: "convert-question"
         },
         {
