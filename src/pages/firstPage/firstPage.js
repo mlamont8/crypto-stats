4860d5ee200d90c78f1fff984fcc1b72030d7efc
Lamont Taylor <mlamont8@hotmail.com>
2021-03-02 13:08:19 -0500

    added call to action

--- src/pages/firstPage/firstPage.js
+++ src/pages/firstPage/firstPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Header from "../../components/header/header";
-// import Footer from "../../components/footer/footer"
+import Footer from "../../components/footer/footer"
 import ChatForm from "../../components/chatForm/ChatForm";
 import dash from "../../images/dash.png"
 import phones from "../../images/phones.png"
@@ -12,9 +12,9 @@ const FirstPage = () => {
 
   return (
     <>
-    <div className="mainContainer">
+    <div className="frontContainer">
       <Header firstLoad={firstLoad} />
-      <div className="frontContainer">
+      <div className="frontTopContainer">
         {modal && <ChatForm initialLoad setModal={setModal} />}
 
         <div className="topFront">
@@ -37,18 +37,23 @@ const FirstPage = () => {
 
 
       </div>
-      <div style={{height: '200px', overflow: 'hidden'}} ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#422168" fillOpacity="1" d="M0,96L80,122.7C160,149,320,203,480,186.7C640,171,800,85,960,69.3C1120,53,1280,107,1360,133.3L1440,160L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg></div>
+      <div className="curve" ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#422168" fillOpacity="1" d="M0,96L80,122.7C160,149,320,203,480,186.7C640,171,800,85,960,69.3C1120,53,1280,107,1360,133.3L1440,160L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg></div>
       <div className="frontInfo">
-      <div><img src={phones} alt="responsive for smartphones"></img></div>
-      <div>
-        <h1>Viewing long - term and short-term forecast</h1>
-        <p>From startups to Fortune 500 companies, Trello is the most visual way for teams to collaborate on any project. Uniquely evolve equity invested value vis-a-vis proactive testing proceduatively create worldwide</p>
-        </div> 
+        <div className="frontInfoImage"><img src={phones} alt="responsive for smartphones"></img></div>
+        <div className='frontInfoText'>
+          <h1>Viewing long - term and short-term forecast</h1>
+          <p>From startups to Fortune 500 companies, Trello is the most visual way for teams to collaborate on any project. Uniquely evolve equity invested value vis-a-vis proactive testing proceduatively create worldwide</p>
+          </div> 
 
     </div>
+
+</div>
+<div className="callToAction">
+      <h2>TRY CRYPTOSTATS FOR FREE</h2>
+      <p>Available on your computer, tablet or mobile.  Runs directly in your browser for free</p>
     </div>
 
-    {/* <Footer /> */}
+    <Footer />
     </>
   );
 };
